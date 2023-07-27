6393991d15ab0c241c5c088ab1955b665d8c38b4
Simon-Bolivar99 <gartem99@yande.ru>
2023-07-27 16:46:44 +0300

    реализация кнопок и простейших операций

--- MainWindow.cpp
+++ MainWindow.cpp
@@ -3,6 +3,7 @@
 #include <QPushButton>
 #include <QGridLayout>
 #include <QLineEdit>
+#include <QString>
 
 MainWindow::MainWindow(QWidget *parent)
     : QWidget(parent)
@@ -46,51 +47,110 @@ void MainWindow::createGui()
         m_btn_minus->setSizePolicy(QSizePolicy::Expanding, QSizePolicy::Expanding);
     m_btn_equal   = new QPushButton("=");
         m_btn_equal->setSizePolicy(QSizePolicy::Expanding, QSizePolicy::Expanding);
+    m_btn_backspace   = new QPushButton("BackSpace");
+        m_btn_backspace->setSizePolicy(QSizePolicy::Expanding, QSizePolicy::Expanding);
 
 
     auto m_layout = new QGridLayout();
         m_layout->setMargin(5);
         m_layout->setSpacing(2);
-        //m_layout->setRowStretch(4,2);
-        m_layout->addWidget(m_line_res,  0, 0, 1, 4);
-        m_layout->addWidget(m_btn_1,     1, 0, 1, 1);
-        m_layout->addWidget(m_btn_2,     1, 1, 1, 1);
-        m_layout->addWidget(m_btn_3,     1, 2, 1, 1);
-        m_layout->addWidget(m_btn_plus,  1, 3, 1, 1);
-        m_layout->addWidget(m_btn_4,     2, 0, 1, 1);
-        m_layout->addWidget(m_btn_5,     2, 1, 1, 1);
-        m_layout->addWidget(m_btn_6,     2, 2, 1, 1);
-        m_layout->addWidget(m_btn_minus, 2, 3, 1, 1);
-        m_layout->addWidget(m_btn_7,     3, 0, 1, 1);
-        m_layout->addWidget(m_btn_8,     3, 1, 1, 1);
-        m_layout->addWidget(m_btn_9,     3, 2, 1, 1);
-        m_layout->addWidget(m_btn_equal, 3, 3, 2, 1);
-        m_layout->addWidget(m_btn_0,     4, 0, 1, 3);
-
+        m_layout->addWidget(m_line_res,       0, 0, 1, 4);
+        m_layout->addWidget(m_btn_1,          1, 0, 1, 1);
+        m_layout->addWidget(m_btn_2,          1, 1, 1, 1);
+        m_layout->addWidget(m_btn_3,          1, 2, 1, 1);
+        m_layout->addWidget(m_btn_plus,       1, 3, 1, 1);
+        m_layout->addWidget(m_btn_4,          2, 0, 1, 1);
+        m_layout->addWidget(m_btn_5,          2, 1, 1, 1);
+        m_layout->addWidget(m_btn_6,          2, 2, 1, 1);
+        m_layout->addWidget(m_btn_minus,      2, 3, 1, 1);
+        m_layout->addWidget(m_btn_7,          3, 0, 1, 1);
+        m_layout->addWidget(m_btn_8,          3, 1, 1, 1);
+        m_layout->addWidget(m_btn_9,          3, 2, 1, 1);
+        m_layout->addWidget(m_btn_equal,      3, 3, 1, 1);
+        m_layout->addWidget(m_btn_backspace,  4, 3, 1, 1);
+        m_layout->addWidget(m_btn_0,          4, 0, 1, 3);
 
 
         setLayout(m_layout);
 }
 
-void MainWindow::buttonHander()
+void MainWindow::buttonHanderNums()
 {
-    m_line_res->setText(qobject_cast<QPushButton*>(sender())->text());
+    auto btn = qobject_cast<QPushButton*>(sender());
+    num_str  += btn->text().toUtf8().constData();
+    line_str += btn->text().toUtf8().constData();
+    m_line_res->clear();
+    m_line_res->setText(QString::fromStdString(line_str));
+}
+
+void MainWindow::buttonHandlerOperation()
+{
+    auto btn = qobject_cast<QPushButton*>(sender());
+    if (btn->text() == "+" && num_str != "0")
+    {
+        res += std::stoi(num_str);
+        line_str += " + ";
+        num_str = "0";
+    }
+
+     if (btn->text() == "-" && num_str != "0")
+    {
+        res -= std::stoi(num_str);
+        line_str += " - ";
+        num_str = "-0";
+
+    }
+     m_line_res->clear();
+     m_line_res->setText(QString::fromStdString(line_str));
+}
+
+void MainWindow::buttonHandlerEqual()
+{
+    if(res != 0 && num_str != "0")
+    {
+        //auto btn = qobject_cast<QPushButton*>(sender());
+        m_line_res->clear();
+        line_str[line_str.length() - 1] == '+'?  line_str += " = " + std::to_string(res += std::stoi(num_str)) : line_str += " = " + std::to_string(res -= std::stoi(num_str));
+        m_line_res->setText(QString::fromStdString(line_str));
+        reset();
+    }
+}
+
+void MainWindow::buttonHandlerBackSpace()
+{
+    if(!(line_str.empty())){
+        if(line_str[line_str.length()] == ' ')
+            line_str.pop_back();
+        m_line_res->clear();
+        line_str.pop_back();
+        m_line_res->setText(QString::fromStdString(line_str));
+}
 }
 
 void MainWindow::connectSignals()
 {
-    connect(m_btn_0, &QPushButton::clicked, this, &MainWindow::buttonHander);
-    connect(m_btn_1, &QPushButton::clicked, this, &MainWindow::buttonHander);
-    connect(m_btn_2, &QPushButton::clicked, this, &MainWindow::buttonHander);
-    connect(m_btn_3, &QPushButton::clicked, this, &MainWindow::buttonHander);
-    connect(m_btn_4, &QPushButton::clicked, this, &MainWindow::buttonHander);
-    connect(m_btn_5, &QPushButton::clicked, this, &MainWindow::buttonHander);
-    connect(m_btn_6, &QPushButton::clicked, this, &MainWindow::buttonHander);
-    connect(m_btn_7, &QPushButton::clicked, this, &MainWindow::buttonHander);
-    connect(m_btn_8, &QPushButton::clicked, this, &MainWindow::buttonHander);
-    connect(m_btn_9, &QPushButton::clicked, this, &MainWindow::buttonHander);
-    connect(m_btn_plus, &QPushButton::clicked, this, &MainWindow::buttonHander);
-    connect(m_btn_minus, &QPushButton::clicked, this, &MainWindow::buttonHander);
-    connect(m_btn_equal, &QPushButton::clicked, this, &MainWindow::buttonHander);
+    connect(m_btn_0,          &QPushButton::clicked, this,      &MainWindow::buttonHanderNums);
+    connect(m_btn_1,          &QPushButton::clicked, this,      &MainWindow::buttonHanderNums);
+    connect(m_btn_2,          &QPushButton::clicked, this,      &MainWindow::buttonHanderNums);
+    connect(m_btn_3,          &QPushButton::clicked, this,      &MainWindow::buttonHanderNums);
+    connect(m_btn_4,          &QPushButton::clicked, this,      &MainWindow::buttonHanderNums);
+    connect(m_btn_5,          &QPushButton::clicked, this,      &MainWindow::buttonHanderNums);
+    connect(m_btn_6,          &QPushButton::clicked, this,      &MainWindow::buttonHanderNums);
+    connect(m_btn_7,          &QPushButton::clicked, this,      &MainWindow::buttonHanderNums);
+    connect(m_btn_8,          &QPushButton::clicked, this,      &MainWindow::buttonHanderNums);
+    connect(m_btn_9,          &QPushButton::clicked, this,      &MainWindow::buttonHanderNums);
+    connect(m_btn_plus,       &QPushButton::clicked, this,      &MainWindow::buttonHandlerOperation);
+    connect(m_btn_minus,      &QPushButton::clicked, this,      &MainWindow::buttonHandlerOperation);
+    connect(m_btn_equal,      &QPushButton::clicked, this,      &MainWindow::buttonHandlerEqual);
+    connect(m_btn_backspace,  &QPushButton::clicked, this,      &MainWindow::buttonHandlerBackSpace);
 }
+
+void MainWindow::reset()
+{
+    line_str = "";
+    res = 0;
+    num_str = "";
+}
+
+
 
--- MainWindow.h
+++ MainWindow.h
@@ -2,6 +2,7 @@
 #define MAINWINDOW_H
 
 #include <QWidget>
+#include <map>
 
 class QPushButton;
 class QLineEdit;
@@ -16,17 +17,32 @@ public:
 
 private:
     void createGui();
-    void buttonHander();
+    void buttonHanderNums();
+    void buttonHandlerOperation();
+    void buttonHandlerEqual();
+    void buttonHandlerBackSpace();
     void connectSignals();
+    void reset();
 
 private:
-    QPushButton *m_btn_0, *m_btn_1 , *m_btn_2,
-    *m_btn_3, *m_btn_4, *m_btn_5, *m_btn_6,
-    *m_btn_7, *m_btn_8, *m_btn_9,
-    *m_btn_plus, *m_btn_minus, *m_btn_equal;
+    std::string num_str  = "";
+    std::string line_str = "";
+
+    int res = 0;
+
+    QPushButton *m_btn_0,     *m_btn_1,     *m_btn_2,         *m_btn_3,
+                *m_btn_4,     *m_btn_5,     *m_btn_6,         *m_btn_7,
+                *m_btn_8,     *m_btn_9,     *m_btn_backspace, *m_btn_plus,
+                *m_btn_minus, *m_btn_equal;
 
     QLineEdit *m_line_res;
 
+
+
+    enum buttonOperation{
+        buttonPlus,
+        buttonMinus
+    };
 
 
 };
